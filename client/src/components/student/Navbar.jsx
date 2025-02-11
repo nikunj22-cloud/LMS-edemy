50bb326df23b73188e22b0f86e1a21a9d8ccb051
Gyanthakur <gps.96169@gmail.com>
2025-02-12 00:42:30 +0530

    feat: added search navbar

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { AppContext } from "../../context/AppContext";
 
 const Navbar = () => {
-	const isCourseListPage = location.pathname.includes("/course_list");
-
+	const isCourseListPage = location.pathname.includes("/course-list");
+	const {navigate, isEducator} = useContext(AppContext);
 	const { openSignIn } = useClerk();
 	const { user } = useUser();
 
@@ -15,7 +16,7 @@ const Navbar = () => {
 				isCourseListPage ? "bg-white" : "bg-cyan-100/70"
 			} `}
 		>
-			<img
+			<img onClick={()=>navigate('/')}
 				src={assets.logo}
 				alt="Logo"
 				className="w-28 lg:w-32  cursor-pointer"
@@ -24,7 +25,7 @@ const Navbar = () => {
 				<div className="flex items-center gap-5">
 					{user && (
 						<>
-							<button>Become Educator</button>|{" "}
+							<button onClick={()=> navigate('/educator')}>{isEducator ? "Educator Dashboard" : "Become Educator" }</button>|{" "}
 							<Link to="/my-enrollments">My Enrollments</Link>
 						</>
 					)}
@@ -46,9 +47,9 @@ const Navbar = () => {
 				<div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
         {user && (
 						<>
-							<button>Become Educator</button>|{" "}
-							<Link to="/my-enrollments">My Enrollments</Link>
-						</>
+						<button onClick={()=> navigate('/educator')}>{isEducator ? "Educator Dashboard" : "Become Educator" }</button>|{" "}
+						<Link to="/my-enrollments">My Enrollments</Link>
+					</>
 					)}
 				</div>
         {
