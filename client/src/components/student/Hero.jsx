d48cb67fb1d4086839fc6b51dba218c8e2499471
Gyanthakur <gps.96169@gmail.com>
2025-02-11 23:08:01 +0530

    feat: added hero

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
+import SearchBar from './SearchBar'
 
 const Hero = () => {
   return (
@@ -12,7 +13,7 @@ const Hero = () => {
 
       <p className='md:hidden text-gray-500 max-w-sm mx-auto'>We bring together world-class instructors to help you achieve your professional goals.</p>
 
-      
+      <SearchBar />
     </div>
   )
 }
