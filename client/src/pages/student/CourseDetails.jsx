2d797efef26924a9a72bea2348bdd85e44d489b3
Gyanthakur <gps.96169@gmail.com>
2025-02-12 12:01:43 +0530

    feat: added course details page

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext';
 
 const CourseDetails = () => {
+
+  const {id} = useParams();
+
+  const [courseData, setCourseData] = useState(null);
+
+  const {allCourses} = useContext(AppContext);
+
+  const fetcheCourseData = async ()=>{
+    const findCourse = allCourses.find(course => course._id === id);
+    setCourseData(findCourse);
+  }
+
+  useEffect(()=>{
+    fetcheCourseData();
+  },[])
+
   return (
     <div>
-      CourseDetails.
+      {/* left column */}
+      <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:placeholder-teal-300 pt-20 text-left'>
+
+        <div className='absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70'></div>
+
+      </div>
+
+      {/* right column */}
+      <div></div>
     </div>
   )
 }
