940edf4fa0c4f4a34cc563272184ee59394b22dc
Gyanthakur <gps.96169@gmail.com>
2025-02-14 01:05:09 +0530

    feat: update server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
 import { clerkWebhooks } from './controllers/webhooks.js';
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
+import connectCloudinay from './configs/cloudinary.js';
 
 // initialize express 
 const app = express();
@@ -10,19 +13,18 @@ const app = express();
 
 // connect to db
 await connectDB();
+await connectCloudinay();
 
 
 // middleware
 app.use(cors());
+app.use(clerkMiddleware())
 
 
 // Route s
-app.get('/', (req,res)=>{
-    res.send("Edemy API is working fine!")
-})
-
+app.get('/', (req,res)=>{res.send("Edemy API is working fine!")})
 app.post('/clerk', express.json(), clerkWebhooks)
-
+app.use('/api/educator', express.json(), educatorRouter);
 
 
 // port
