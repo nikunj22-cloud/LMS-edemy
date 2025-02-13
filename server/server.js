9c9792b87a641799345d384e5a8acab0f09d0799
Gyanthakur <gps.96169@gmail.com>
2025-02-13 23:37:31 +0530

    feat: added user model

--- server/server.js
+++ server/server.js
@@ -1,18 +1,29 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './configs/mongodb.js';
+import { clerkWebhooks } from './controllers/webhooks.js';
 
 // initialize express 
 const app = express();
+
+
+// connect to db
+await connectDB();
+
 
 // middleware
 app.use(cors());
 
 
-// Route 
+// Route s
 app.get('/', (req,res)=>{
     res.send("Edemy API is working fine!")
 })
+
+app.post('/clerk', express.json(), clerkWebhooks)
+
+
 
 // port
 const PORT = process.env.PORT || 3000;
