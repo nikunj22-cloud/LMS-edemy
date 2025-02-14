c33dbbe37c07b2586c33c561ca262ea3078978f7
Gyanthakur <gps.96169@gmail.com>
2025-02-15 00:56:18 +0530

    feat: modified server

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
-import { clerkWebhooks } from './controllers/webhooks.js';
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js';
 import educatorRouter from './routes/educatorRoutes.js';
 import { clerkMiddleware } from '@clerk/express';
 import connectCloudinay from './configs/cloudinary.js';
 import courseRouter from './routes/courseRoute.js';
+import userRouter from './routes/userRoutes.js';
 
 // initialize express 
 const app = express();
@@ -22,11 +23,13 @@ app.use(cors());
 app.use(clerkMiddleware())
 
 
-// Route s
+// Routes
 app.get('/', (req,res)=>{res.send("Edemy API is working fine!")})
 app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter);
 app.use('/api/course', express.json(), courseRouter);
+app.use('/api/user', express.json(), userRouter);
+app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 
 
 
