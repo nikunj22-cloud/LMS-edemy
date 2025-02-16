ab01df02a139133995b94d0bb26ab3b6f8bd43e6
Gyanthakur <gps.96169@gmail.com>
2025-02-16 17:27:10 +0530

    feat: update model

--- server/models/Course.js
+++ server/models/Course.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose";
 
 const lectureSchema = new mongoose.Schema({
     lectureId: {type: String, required: true},
+    // lectureId: {type: String},
     lectureTitle: {type: String, required: true},
     lectureDuration: {type: Number, required: true},
-    lectureurl: {type: String, required: true},
+    // lectureurl: {type: String, required: true},
+    lectureUrl: {type: String, required: true},
     isPreviewFree: {type: Boolean, required: true},
     lectureOrder: {type: Number, required: true},
 
@@ -13,20 +15,24 @@ const lectureSchema = new mongoose.Schema({
 
 const chapterSchema = new mongoose.Schema({
     chapterId: {type: String, required: true},
-    chapterorder: {type: Number, required: true},
+    chapterOrder: {type: Number, required: true},
+    // chapterorder: {type: Number, required: true},
+    chapterTitle: {type: String, required: true},
     chapterContent: [lectureSchema]
 },{_id: false});
 
 const courseSchema = new mongoose.Schema({
     courseTitle: {type: String, required: true},
     courseDescription: {type: String, required: true},
-    courseThumbnail: {type: String, required: true},
+    courseThumbnail: {type: String},
     coursePrice: {type: Number, required: true},
-    isPublished: {type: Boolean, required: true},
+    // isPublished: {type: Boolean, default: true},
+    isPublished: { type: Boolean, default: true },
+
     discount: {type: Number, required: true, min:0, max: 100},
     courseContent: [chapterSchema],
     courseRatings:[
-        {userId: {type:String}, rating: {type: Number, min:1, max: 5}}
+        {userId: {type:String}, rating: {type: Number, min: 1, max: 5}}
     ],
     educator:{ type: String, ref: 'User', required: true},
     enrolledStudents: [
