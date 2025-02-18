5fea50fa21e99eb6e7d6c4f8c33f1f71c4ab6e51
Gyanthakur <gps.96169@gmail.com>
2025-02-18 13:00:22 +0530

    feat: added jpg passing

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.JPG'],
   server: {
     hmr: {
       overlay: false, // Disable error overlay
