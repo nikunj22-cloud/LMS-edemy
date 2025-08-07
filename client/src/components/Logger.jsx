1b67b6757270fc3433f1277dfe9232be74223c9e
Gyanthakur <gps.96169@gmail.com>
2025-08-07 18:56:09 +0530

    update

--- client/src/components/Logger.jsx
+++ client/src/components/Logger.jsx
@@ -11,9 +11,13 @@ export default function Logger() {
 				headers: {
 					"content-type": "application/json",
 				},
+				// body: JSON.stringify({
+				// 	clientSecret: "d34314a1-ef9e-46e9-85c1-0d75e48b5325",
+				// 	applicationId: "e7d77b0a-e665-4686-a17f-e7e633aec624",
+				// }),
 				body: JSON.stringify({
-					clientSecret: "d34314a1-ef9e-46e9-85c1-0d75e48b5325",
-					applicationId: "e7d77b0a-e665-4686-a17f-e7e633aec624",
+					clientSecret: "7284daa1-9273-4065-ab8e-61a245a24956",
+					applicationId: "60f8af1b-0ab7-42eb-9791-8be92f36da4e",
 				}),
 			};
 			const res = await fetch(
@@ -33,7 +37,7 @@ export default function Logger() {
 	return (
 		
         <div className="w-fit m-auto font-semibold gap-3 bg-gradient-to-b from-cyan-100/20 text-lg sm:text-xs p-3 sm:p-2 rounded-md shadow-md z-50">
-        Visitors: <span className="text-green-500"> {12053 + visitors} </span>
+        Visitors: <span className="text-green-500"> {visitors} </span>
       </div>
     //     <div className="w-fit m-auto  bg-gradient-to-r from-blue-500 to-purple-600 text-white text-sm sm:text-xs p-3 sm:p-2 rounded-md shadow-lg z-50">
     //     Visitors: {visitors}
